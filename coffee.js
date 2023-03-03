7896beba5b4958d05385b33775c6249e5422eb9f
Kuppuch <kuppuchr@gmail.com>
2023-03-03 13:26:09 +0300

    particle update

---
+++ coffee.js
@@ -0,0 +1,91 @@
+var timer = document.getElementById("timer");
+
+let particleOffset = 0;
+let particleSystem;
+var positions = [];
+let coffeeProcess = false;
+
+function coffeeInit() {
+    const particles = 5000;
+    const radius = 0.5;
+    geometryP = new THREE.BufferGeometry();
+
+
+    const colors = [];
+    const sizes = [];
+
+
+
+    uniforms = {
+        pointTexture: {
+            value: new THREE.TextureLoader().load('tex/particle.png')
+        }
+    };
+
+    const shaderMaterial = new THREE.ShaderMaterial({
+
+        uniforms: uniforms,
+        vertexShader: document.getElementById('vertexshader').textContent,
+        fragmentShader: document.getElementById('fragmentshader').textContent,
+
+        //blending: THREE.AdditiveBlending,
+        //depthTest: false,
+        transparent: true,
+        vertexColors: true
+
+    });
+
+    const color = new THREE.Color();
+
+    for (let i = 0; i < particles; i++) {
+
+        positions.push((Math.random() * 2 - 1) * radius);
+        positions.push((Math.random() * 2 - 1) * radius * 5 + 1 - particleOffset);
+        positions.push((Math.random() * 2 - 1) * radius + 2);
+
+        color.setHSL(i / particles, 1.0, 0.5);
+
+        colors.push(0.6, 0.6, 0.4);
+
+        sizes.push(0.2);
+
+    }
+
+    geometryP.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    geometryP.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    geometryP.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setUsage(THREE.DynamicDrawUsage));
+
+    particleSystem = new THREE.Points(geometryP, shaderMaterial);
+    particleSystem.name = "particleSystem";
+}
+
+async function pourCoffee() {
+    if (coffeeProcess) {
+        return;
+    }
+    coffeeProcess = true;
+
+    if (positions.length < 1) {
+        coffeeInit();
+    }
+
+    scene.add(particleSystem);
+
+    //await timerUpdate();
+
+    setTimeout(destroyCoffee, 5000);
+}
+
+function destroyCoffee() {
+    let coffeeStream = scene.getObjectByName(particleSystem.name);
+    scene.remove(coffeeStream);
+    coffeeProcess = false;
+}
+
+function timerUpdate() {
+    for (let i = 6; i > 0; i--) {
+        timer.innerHTML = i;
+        //console.log(i);
+        setTimeout(timerUpdate, 1000, i)
+    }
+}
--- main.js
+++ main.js
@@ -362,9 +362,6 @@ var pourer; {
     scene.add(glass);
 }
 
-let particleOffset = 0;
-let particleSystem;
-const positions = [];
 
 // Частицы
 {
@@ -390,62 +387,6 @@ const positions = [];
             }
         }
     }
-
-
-    const particles = 1000;
-    const radius = 0.5;
-    geometryP = new THREE.BufferGeometry();
-
-    
-    const colors = [];
-    const sizes = [];
-
-    
-
-    uniforms = {
-
-        pointTexture: {
-            value: new THREE.TextureLoader().load('tex/particle.png')
-        }
-
-    };
-
-    const shaderMaterial = new THREE.ShaderMaterial({
-
-        uniforms: uniforms,
-        vertexShader: document.getElementById('vertexshader').textContent,
-        fragmentShader: document.getElementById('fragmentshader').textContent,
-
-        //blending: THREE.AdditiveBlending,
-        //depthTest: false,
-        transparent: true,
-        vertexColors: true
-
-    });
-
-    const color = new THREE.Color();
-
-    for (let i = 0; i < particles; i++) {
-
-        positions.push((Math.random() * 2 - 1) * radius);
-        positions.push((Math.random() * 2 - 1) * radius*5 + 1 - particleOffset);
-        positions.push((Math.random() * 2 - 1) * radius + 2);
-
-        color.setHSL(i / particles, 1.0, 0.5);
-
-        colors.push(0.6, 0.6, 0.4);
-
-        sizes.push(0.2);
-
-    }
-
-    geometryP.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-    geometryP.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-    geometryP.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setUsage(THREE.DynamicDrawUsage));
-
-    particleSystem = new THREE.Points(geometryP, shaderMaterial);
-
-    scene.add(particleSystem);
 
 }
 
@@ -522,17 +463,19 @@ function render(time) {
 
     dynamo();
     controls.update();
-    partUpdate();
+    if (positions.length > 0) {
+        coffeeUpdate();
+    }
 
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
 }
 
-function partUpdate() {
+function coffeeUpdate() {
     let len = particleSystem.geometry.attributes.position.array.length
     for (let i = 1; i < len; i += 3) {
-        particleSystem.geometry.attributes.position.array[i] -= 0.05;
+        particleSystem.geometry.attributes.position.array[i] -= 0.1;
         if (particleSystem.geometry.attributes.position.array[i] < -1) {
             particleSystem.geometry.attributes.position.array[i] = 3
         }
